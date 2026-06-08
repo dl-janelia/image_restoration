847411ff83a0299de5d9bd0a9ea42c4165c6e127
federico-carrara <federico1.carrara@mail.polimi.it>
2026-06-08 05:36:52 +0200

    fixes

--- 02_Noise2Void/solution.py
+++ 02_Noise2Void/solution.py
@@ -273,7 +273,7 @@ careamist.train(train_data=train_images_path, val_data=validation_images_path)
 #
 # Remember the configuration? Didn't we set `logger` to `tensorboard`? Then we can visualize the loss curve!
 #
-# Open Tensorboard in the terminal (check Task 5 in 01_CARE) to monitor training. 
+# Open Tensorboard in the terminal (check Task 6 in 01_CARE) to monitor training. 
 # Logs for this model are stored in the `02_Noise2Void/tb_logs/` folder.
 # </div>
 #
